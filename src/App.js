16ca5e7177613b4b70319afa83c005636ace44b9
Sophie <s.ischenko1990@gmail.com>
2024-10-17 12:22:16 +0200

    fixed toastys

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import TodoList from "./TodoList";
 import "./styles.css";
-import { ToastContainer, toast } from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import TodoItem from './TodoItem';
 import TodoInput from './TodoInput';
-import { ToastContainer, toast } from 'react-toastify';
+import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 // const defaultTodos = [
